cdde1cf72bc635336cbd848a959bf2d8a1b10e80
John <jmr16u@my.fsu.edu>
2019-07-04 12:57:46 -0400

    Converts SQL to pandas dataframe, and supporting functions - added group chat functions

--- gather.py
+++ gather.py
@@ -11,12 +11,13 @@ path = "/Users/john/Desktop/chat.db"
 
 def populate_frame(file):
     conn = sqlite3.connect(file)
-    df = pd.read_sql_query("select * from message limit 26675;", conn)
+    df = pd.read_sql_query("select * from message;", conn)
     handle = pd.read_sql_query("select * from handle;", conn)
     row = 0
     for message in df["handle_id"]:
         df.iloc[row, df.columns.get_loc('handle_id')] = handle.iloc[message-1, handle.columns.get_loc('id')]
         row += 1
+    clean_null(df)
     return df
 
 
@@ -35,7 +36,20 @@ def is_me(df):  # returns dateframe of text sent by you
 
 
 def is_emote(df): # returns dataframe of emotes
-    return df.query('is_emote == 1 ')
+    return df.query('is_emote == 1')
+
+
+def find_convo(df, number):  # returns text conversation with number, where number includes the country code
+    return df.query('handle_id == @number')
+
+
+def is_groupchat(df, roomname=''):  # returns a specified group chat if roomname is available, else returns df with all
+    # group chats.
+    df = df.query('cache_roomname != NULL')
+    if not roomname:
+        return df.query('cache_roomname == @roomname')
+    else:
+        return df
 
 
 if __name__ == '__main__':
@@ -43,6 +57,6 @@ if __name__ == '__main__':
     # print(df[['text', 'handle_id']])
     df = clean_null(df)
     # print(df[['text', 'handle_id']])
-    df = is_me(df)
+    df = is_emote(df)
     df = df[['text', 'handle_id']]
     print(df)
